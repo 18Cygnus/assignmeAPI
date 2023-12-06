a404c1865e32a75286a30dab4600a7952ae1d274
18Cygnus <aryasanjaya001@gmail.com>
2023-12-06 12:59:15 +0700

    timeline

--- showCompletedAndLate.php
+++ showCompletedAndLate.php
@@ -1,15 +1,15 @@
 <?php
-//showTodo
+//showCompletedAndLate
+
 require_once 'connection/connection.php';
 
 $result = array();
-$connection = new Connect(); // Buat instance dari kelas Connect
-$con = $connection->dbConn(); // Panggil method dbConn untuk koneksi ke database
+$connection = new Connect();
+$con = $connection->dbConn();
 
 if ($con) {
     $email = $_POST['Email'];
 
-    // Query untuk mendapatkan UserId berdasarkan Email dari tabel users
     $sqlUserId = "SELECT UserId FROM users WHERE Email = ?";
     $stmtUserId = mysqli_prepare($con, $sqlUserId);
     mysqli_stmt_bind_param($stmtUserId, "s", $email);
@@ -18,34 +18,21 @@ if ($con) {
 
     if (mysqli_num_rows($resultUserId) > 0) {
         $row = mysqli_fetch_assoc($resultUserId);
-        $userId = $row['UserId']; 
-
-        // Query untuk mendapatkan daftar kelas yang di-join oleh pengguna berdasarkan UserId
+        $userId = $row['UserId'];
     
-        $sql = "SELECT 
-        t.TaskId,
-        t.TaskName,
-        t.TaskDesc,
-        t.DueDate,
-        t.Attachment,
-        ts.status,
-        u.UserId
-    FROM 
-        tasks t
-    JOIN 
-        user_classes uc ON t.ClassId = uc.ClassId
-    JOIN 
-        users u ON uc.UserId = u.UserId
-    LEFT JOIN 
-        task_submits ts ON t.TaskId = ts.TaskId AND u.UserId = ts.UserId
-    WHERE u.UserId = ?
-        AND ts.status = 'Completed'
-        AND t.DueDate < NOW()";       
+        // Modify your query to filter tasks based on UserId
+        $sql = "SELECT ts.*, t.TaskName, t.TaskDesc, t.DueDate, t.ClassId, t.Attachment
+                FROM users u
+                JOIN user_classes uc ON u.UserId = uc.UserId
+                JOIN tasks t ON uc.ClassId = t.ClassId
+                LEFT JOIN task_submits ts ON u.UserId = ts.UserId AND ts.TaskId = t.TaskId
+                WHERE u.Email = ?
+                AND ts.SubmitDate > t.DueDate";         
         $stmt = mysqli_prepare($con, $sql);
-        mysqli_stmt_bind_param($stmt, "i", $userId);
+        mysqli_stmt_bind_param($stmt, "i", $email);
         mysqli_stmt_execute($stmt);
         $result = mysqli_stmt_get_result($stmt);
-
+    
         if (mysqli_num_rows($result) > 0) {
             $userClasses = array();
             while ($row = mysqli_fetch_assoc($result)) {
@@ -63,4 +50,5 @@ if ($con) {
 }
 
 $con->close();
+
 ?>
